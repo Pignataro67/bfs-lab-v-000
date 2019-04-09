e083361dd71a30fa2ddfdcd079c51e15ce9d95a4
Pat  Pignataro <pat6284@msn.com>
2019-04-09 00:20:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ function bfs(rootNode, vertices, edges){
 function findAdjacent(rootNode, vertices, edges) {
   const pairs = edges.filter(e => e.includes(rootNode))
   let names = pairs.map(e => e.find(n => n != rootNode))
-  return vertices.filter(v => names.includes(v.name) && v.distance == null)
+  return vertices.filter(v => names.includes(v.name) && v.distance === null)
 }
 
 function markDistanceAndPredecessor(predecessor, adjacentNodes) {
